e9fa6ee5c0ca945a99f9cc1b933180f98bc6fa1b
natanbravo <nobprogrammer8@gmail.com>
2022-05-05 22:27:00 -0300

    Challenge Update

---
+++ challenge08/README.md
@@ -0,0 +1,14 @@
+HERE IS MY RESOLUTION OF THIS CHALLENGE 
+
+
+
+<a target="_blank"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/835bf6142914517.627478422e1c7.png" target="_blank"></a>
+
+
+
+
+HERE IS MY SECOND RESOLUTION OF THIS CHALLENGE 
+
+
+
+<a target="_blank"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/43a7cc142914517.627478422e8f0.png" target="_blank"></a>
---
+++ challenge08/challenge08.js
@@ -0,0 +1,36 @@
+//Write s function that receives an array as a parameter;
+//And removes the first 3 array elements;
+//If an array have less than 3 elements, return an empty array;
+
+function  myFunction(a){
+    if (a.length <= 3){
+        return a = [];
+    }
+    else if(a.length>=3){
+        return a.slice(3);
+    }
+
+}
+
+console.log(myFunction([1,2,3,4,5])); //[4,5]
+console.log(myFunction([5,4,3,2,1,0])); //[2,1,0]
+console.log(myFunction([10,20,30])); //[]
+console.log(myFunction([99,100])); //[]
+
+//=======================//=======================//=======================//=======================//=======================
+
+
+
+
+
+
+//The second way to solve this problem in a simplified way;
+
+function myFunctionTwo(a){
+    return a.length > 3 ? a.slice(3) : [];
+}
+
+console.log(myFunctionTwo([1,2,3,4,5])); //[4,5]
+console.log(myFunctionTwo([5,4,3,2,1,0])); //[2,1,0]
+console.log(myFunctionTwo([10,20,30])); //[]
+console.log(myFunctionTwo([99,100])); //[]
